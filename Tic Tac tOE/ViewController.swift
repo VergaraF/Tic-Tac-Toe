c628873db550328b03e84c7808111e7a8f357c1e
Fabian Vergara <vergarf@gmail.com>
2016-06-04 13:32:11 -0400

    Few improvements End of the game label and button (play again) added Added helper functions!

--- Tic Tac tOE/ViewController.swift
+++ Tic Tac tOE/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var button: UIButton!
     
     var currentPlayer = 1 // even for x, odd for O
+    @IBOutlet var endOfTheGameLabel: UILabel!
+    @IBOutlet var playAgainButton: UIButton!
+    @IBOutlet var button: UIButton!
     
     var gameState = [0 ,0 ,0,
                      0, 0, 0,
@@ -48,7 +49,7 @@ class ViewController: UIViewController {
                                 
                                 [      2,
                                     4,
-                                 6] ]
+                                 6]     ]
                                 
                                 
     
@@ -63,34 +64,50 @@ class ViewController: UIViewController {
 
             }
             currentPlayer += 1
+            endOfTheGameListener()
+        }else if !gameActive{
+            setLabelAndShowButton("It's a DRAW", colour: UIColor.whiteColor())
         }
         
-        endOfTheGameListener()
-
-        
-        
+       // endOfTheGameListener()
     }
     
     private func endOfTheGameListener(){
       //  print ("End of the game listener executed")
         for combinations in winningCombinations{
+            
             if (gameState[combinations[0]] != 0 &&
                 gameState[combinations[0]] == gameState[combinations[1]] &&
                 gameState[combinations[1]] == gameState[combinations[2]]){
+                
                 if (gameState[combinations[0]] == 1){
-                    print("Player one won!")
+                    setLabelAndShowButton("Player one WON!", colour: UIColor.greenColor())
                 }else{
-                    print("Player two won!")
+                    setLabelAndShowButton("Play two WON!", colour: UIColor.greenColor())
                 }
+                
                 self.gameActive = false
             }
         }
     }
+    
+    func setLabelAndShowButton(label: String, colour: UIColor){
+        self.endOfTheGameLabel.text = label
+        self.endOfTheGameLabel.textColor = colour
+        self.endOfTheGameLabel.hidden = false
+        self.playAgainButton.hidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.endOfTheGameLabel.hidden = true
+        self.playAgainButton.hidden   = true
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
